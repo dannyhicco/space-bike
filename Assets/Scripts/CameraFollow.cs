a4fd6c8e428095b47bc7b470d9ecd72789096ab9
johntringham <john@tringham.co.uk>
2015-08-23 23:45:09 +0100

    Modified CameraFollow to have a warpspeed effect. Messed with lighting and parameters.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,14 +7,20 @@ public class CameraFollow : MonoBehaviour {
 	public float MouseSensitivity = 2.0f;
 	public GameObject target;
 
+	public float FieldOfViewMagnifier;
+	public float FieldOfViewMax;
+
 	public float LerpConstant = 100f;
 	private Rigidbody targetRigidBody;
+
+	private Camera camera;
 
 	Vector3 offset;
 
 	void Start () {
 		offset = this.transform.position - target.transform.position;
 		targetRigidBody = target.GetComponent<Rigidbody>();
+		camera = this.GetComponent<Camera>();
 	}
 	
 	void Update () {
@@ -23,5 +29,10 @@ public class CameraFollow : MonoBehaviour {
 		this.transform.rotation = Quaternion.LookRotation(-offset);
 
 		this.transform.position = Vector3.Lerp (this.transform.position, target.transform.position + offset, LerpConstant);
+
+		var flatVelVect = new Vector2(targetRigidBody.velocity.x, targetRigidBody.velocity.z);
+
+		camera.fieldOfView = Mathf.Clamp(50 + flatVelVect.magnitude * FieldOfViewMagnifier, 0, FieldOfViewMax);
 	}
 }
+
